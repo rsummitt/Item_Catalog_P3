f27b6c038f9d8800d99949eecc8bd29900508f29
Robert Summitt <robert.summitt@gmail.com>
2015-05-10 21:00:14 -0500

    Broken authorization

--- application.py
+++ application.py
@@ -1,22 +1,60 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+import os
+from database_setup import Base, Category, Item
+from flask import Flask, render_template, request, redirect, jsonify, url_for, session, g
+from functools import wraps
+from requests_oauthlib import OAuth2Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Item
-
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+db_session = DBSession()
+
+client_id = "46a8d3ab8c19a99a4ba4"
+client_secret = "2e17f44c5b3ce05c3e4210ea3ae864ab61cdc0eb"
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
+
+@app.before_first_request
+def session_setup():
+    session['oauth_state'] = None
+    session['oauth_token'] = None
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session['oauth_state'] is None or session['oauth_token'] is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# Todo: Create a login endpoint
+@app.route('/login/')
+def login():
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+# Todo: Create a github-callback
+@app.route('/github-callback')
+def github_callback():
+    github = OAuth2Session(client_id, state=session['oauth_state'])
+    token = github.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
+    session['oauth_token'] = token
+    return redirect(url_for('get_catalog'))
 
 
 @app.route('/')
 @app.route('/catalog/')
+@login_required
 def get_catalog():
-    categories = session.query(Category).all()
+    categories = db_session.query(Category).all()
     return render_template('categories.html', categories=categories)
 
 
@@ -25,8 +63,8 @@ def add_category():
     if request.method == 'POST':
         if request.form['name']:
             category = Category(name=request.form['name'])
-            session.add(category)
-            session.commit()
+            db_session.add(category)
+            db_session.commit()
             return redirect(url_for('get_catalog'))
     else:
         return render_template('add_category.html')
@@ -34,23 +72,23 @@ def add_category():
 
 @app.route('/catalog/category/<int:category_id>/delete/')
 def delete_category(category_id):
-    category = session.query(Category).filter_by(id=category_id).one()
-    session.delete(category)
-    session.commit()
+    category = db_session.query(Category).filter_by(id=category_id).one()
+    db_session.delete(category)
+    db_session.commit()
     return redirect(url_for('get_catalog'))
 
 
 @app.route('/catalog/category/<int:category_id>/items/')
 def get_category_items(category_id):
-    category = session.query(Category).filter_by(id=category_id).one()
-    category_items = session.query(Item).filter_by(category_id=category.id).all()
+    category = db_session.query(Category).filter_by(id=category_id).one()
+    category_items = db_session.query(Item).filter_by(category_id=category.id).all()
     return render_template('category_items.html', category=category, category_items=category_items)
 
 
 @app.route('/catalog/category/<int:category_id>/item/<int:item_id>/')
 def get_item(category_id, item_id):
-    category = session.query(Category).filter_by(id=category_id).one()
-    item = session.query(Item).filter_by(category_id=category.id, id=item_id).one()
+    category = db_session.query(Category).filter_by(id=category_id).one()
+    item = db_session.query(Item).filter_by(category_id=category.id, id=item_id).one()
     return render_template('item.html', category=category, item=item)
 
 
@@ -58,53 +96,55 @@ def get_item(category_id, item_id):
 def add_item():
     if request.method == 'POST':
         if request.form['name'] and request.form['description'] and request.form['category']:
-            category = session.query(Category).filter_by(id=request.form['category']).one()
+            category = db_session.query(Category).filter_by(id=request.form['category']).one()
             item = Item(name=request.form['name'], description=request.form['description'], category_id=category.id)
-            session.add(item)
-            session.commit()
+            db_session.add(item)
+            db_session.commit()
             return redirect(url_for('get_item', category_id=category.id, item_id=item.id))
     else:
-        categories = session.query(Category).all()
+        categories = db_session.query(Category).all()
         return render_template('add_item.html', categories=categories)
 
 
 @app.route('/catalog/item/<int:item_id>/edit/', methods=['GET', 'POST'])
 def edit_item(item_id):
-    item = session.query(Item).filter_by(id=item_id).one()
+    item = db_session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         if request.form['name'] and request.form['description'] and request.form['category']:
-            category = session.query(Category).filter_by(id=request.form['category']).one()
+            category = db_session.query(Category).filter_by(id=request.form['category']).one()
             item.name = request.form['name']
             item.description = request.form['description']
             item.category_id = category.id
-            session.commit()
+            db_session.commit()
             return redirect(url_for('get_item', category_id=category.id, item_id=item.id))
     else:
-        categories = session.query(Category).all()
+        categories = db_session.query(Category).all()
         return render_template('edit_item.html', categories=categories, item=item)
 
 
 @app.route('/catalog/item/<int:item_id>/delete/')
 def delete_item(item_id):
-    item = session.query(Item).filter_by(id=item_id).one()
+    item = db_session.query(Item).filter_by(id=item_id).one()
     category_id = item.category_id
-    session.delete(item)
-    session.commit()
+    db_session.delete(item)
+    db_session.commit()
     return redirect(url_for('get_category_items', category_id=category_id))
 
 
 @app.route('/api/catalog')
 def categories_json():
-    categories = session.query(Category).all()
+    categories = db_session.query(Category).all()
     results = []
     for c in categories:
         result = {'id': c.id, 'name': c.name, 'Items': []}
-        for i in session.query(Item).filter_by(category_id=c.id):
+        for i in db_session.query(Item).filter_by(category_id=c.id):
             result['Items'].append(i.serialize)
         results.append(result)
     return jsonify(Categories=results)
 
 
 if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.debug = True
+    app.secret_key = "Dev Key"
     app.run(host='0.0.0.0', port=8080)
--- pg_config.sh
+++ pg_config.sh
@@ -5,6 +5,7 @@ apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install bleach
 pip install oauth2client
+pip install requests_oauthlib
 pip install requests
 pip install httplib2
 su postgres -c 'createuser -dRS vagrant'
